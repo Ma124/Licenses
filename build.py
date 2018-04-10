42fea1bda1e04ed0e2ed226be6b648c9b4bcd310
MA-PC\Ma_124 <ma_124@outlook.com>
2018-04-10 20:20:55 +0200

    modified build sys, moved templates to templates/, added indexes

--- build.py
+++ build.py
@@ -12,24 +12,51 @@ def get_name(p):
     return name
 
 
-f = open('page-template.html', 'r')
-templ = Template(f.read())
+f = open('templates/page-template.html', 'r')
+tmpl_page = Template(f.read())
+f.close()
+
+f = open('templates/index-template.html', 'r')
+tmpl_index = Template(f.read())
+f.close()
+
+f = open('templates/index-item-template.html', 'r')
+tmpl_index_item = Template(f.read())
 f.close()
 
 meta = ConfigParser()
 meta._interpolation = ExtendedInterpolation()
 meta.read('meta.ini')
 
+index = ''
+
 for root, subdirs, files in os.walk('html'):
     for f in files:
+        if not f.endswith('.html') or f == 'index.html':
+            continue
         model = dict()
 
         fo = open('html/' + f, 'r')
         model['license_text'] = fo.read()
         fo.close()
 
-        model['license_name'] = meta[get_name(f)]['name']
+        id = model['license_id'] = get_name(f)
+        model['license_name'] = meta[id]['name']
+
+        index += tmpl_index_item.substitute(model)
 
         fo = open('page/' + get_name(f) + '.html', 'w')
-        fo.write(templ.substitute(model))
+        fo.write(tmpl_page.substitute(model))
         fo.close()
+
+model = dict()
+model['items'] = index
+index = tmpl_index.substitute(model)
+
+f = open('page/index.html', 'w')
+f.write(index)
+f.close()
+
+f = open('html/index.html', 'w')
+f.write(index)
+f.close()
